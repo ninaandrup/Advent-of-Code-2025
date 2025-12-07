d642464646ff3a1169af84b61ebf563d4a1378c2
Nina <au674305@uni.au.dk>
2025-12-07 17:26:08 +0100

    Add solution for day 7 part 1

--- src/AoC/Day07.hs
+++ src/AoC/Day07.hs
@@ -1,9 +1,28 @@
 module AoC.Day07 (solution) where
 
+import Data.List (elemIndex)
+import qualified Data.Maybe as Maybe
+import qualified Data.Set as Set
+import Debug.Trace (trace)
+import qualified Utils.Grid as Grid
 import qualified Utils.Utils as Utils
 
 part1 :: Utils.SolutionSingle
-part1 _ = 0
+part1 input =
+  let startPos = Maybe.fromJust . elemIndex 'S' $ head input
+      grid = Grid.fromGridList . map (map (== '^')) $ tail input
+      dfsVisited = Grid.dfs (0, startPos) (adjacent grid)
+      hitSplitter (x, y) = Maybe.fromMaybe False $ Grid.getCellSafe (x + 1, y) grid
+   in length . filter hitSplitter $ Set.toList dfsVisited
+  where
+    adjacent :: Grid.Grid Bool -> Grid.Adjacent
+    adjacent grid (x, y) =
+      let below = (x + 1, y)
+          possibleAdj = case Grid.getCellSafe below grid of
+            Just True -> [(x + 1, y - 1), (x + 1, y + 1)]
+            Just False -> [(x + 1, y)]
+            Nothing -> []
+       in filter (`Grid.validCell` grid) possibleAdj
 
 part2 :: Utils.SolutionSingle
 part2 _ = 0
--- src/Utils/Grid.hs
+++ src/Utils/Grid.hs
@@ -1,5 +1,6 @@
 module Utils.Grid where
 
+import qualified Data.Set as Set
 import qualified Data.Vector as V
 
 -- Row-major 2D grid
@@ -18,7 +19,14 @@ instance Functor Grid where
   fmap f grid = Grid {width = width grid, height = height grid, cells = V.map f (cells grid)}
 
 instance (Show a) => Show (Grid a) where
-  show grid = show (width grid, height grid, show (cells grid))
+  show = concatMap ((++ "\n") . show) . showGrid
+
+showGrid :: (Show a) => Grid a -> V.Vector (V.Vector a)
+showGrid grid =
+  V.fromList [V.slice (i * width grid) (width grid) (cells grid) | i <- [0 .. height grid - 1]]
+
+emptyGrid :: Grid a
+emptyGrid = Grid {width = 0, height = 0, cells = V.empty}
 
 fromGridList :: [[a]] -> Grid a
 fromGridList grid
@@ -30,6 +38,8 @@ fromGridList grid
           cells = V.fromList (concat grid)
         }
 
+type CellPos = (Int, Int)
+
 -- Convert (x, y) coordinates to (row, column) index
 --
 -- [[a00, a01, a02, a03],
@@ -40,5 +50,35 @@ fromGridList grid
 -- (0,1) -> a01
 -- (1,0) -> a10
 -- (2,3) -> a23
-getCell :: (Int, Int) -> Grid a -> a
+getCell :: CellPos -> Grid a -> a
 getCell (x, y) grid = cells grid V.! (x * width grid + y)
+
+getCellSafe :: CellPos -> Grid a -> Maybe a
+getCellSafe (x, y) grid
+  | validCell (x, y) grid = Just $ getCell (x, y) grid
+  | otherwise = Nothing
+
+validCell :: CellPos -> Grid a -> Bool
+validCell (x, y) grid
+  | height grid <= x = False
+  | y < 0 = False
+  | width grid <= y = False
+  | otherwise = True
+
+type Visited = Set.Set CellPos
+
+type Adjacent = (CellPos -> [CellPos])
+
+dfs :: CellPos -> Adjacent -> Visited
+dfs start adj = dfsIter start Set.empty
+  where
+    dfsIter :: CellPos -> Visited -> Visited
+    dfsIter cellPos visited =
+      foldl
+        ( \acc adjPos ->
+            if Set.member adjPos acc
+              then acc
+              else dfsIter adjPos acc
+        )
+        (Set.insert cellPos visited)
+        $ adj cellPos
