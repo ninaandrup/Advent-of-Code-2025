e1a60685f7005e7a67050b46d659c3e73a98bf01
Nina <au674305@uni.au.dk>
2025-12-23 21:28:13 +0100

    Solve day 9 (part 2) and day 10 (part 1)

--- src/AoC/Day09.hs
+++ src/AoC/Day09.hs
@@ -2,16 +2,22 @@
 
 module AoC.Day09 (solution) where
 
+import Data.List (maximumBy)
+import qualified Data.Maybe as M
 import Debug.Trace (trace)
 import qualified Utils.Utils as Utils
+
+type Point = (Int, Int)
 
 pairs :: [a] -> [(a, a)]
 pairs (x : xs) = map (x,) xs ++ pairs xs
 pairs [] = []
 
-area :: (Int, Int) -> (Int, Int) -> Int
-area (x1, y1) (x2, y2) =
-  abs (x1 - x2 + 1) * abs (y1 - y2 + 1)
+area :: Point -> Point -> Int
+area (x1, y1) (x2, y2) = width * height
+  where
+    width = abs (x2 - x1) + 1
+    height = abs (y2 - y1) + 1
 
 part1 :: Utils.SolutionSingle
 part1 input =
@@ -20,36 +26,90 @@ part1 input =
       areas = map (uncurry area) pointPairs
    in maximum areas
 
+type HorizontalSegment = ((Int, Int), Int) -- ((x1, x2), y)
+
+horizontalSegments :: [Point] -> [HorizontalSegment]
+horizontalSegments [] = []
+horizontalSegments [_] = []
+horizontalSegments ((x1, y1) : p2@(x2, y2) : ps)
+  | y1 == y2 = ((minX, maxX), y1) : horizontalSegments ps
+  | otherwise = horizontalSegments (p2 : ps)
+  where
+    minX = min x1 x2
+    maxX = max x1 x2
+
+shootRayVerticallyFrom ::
+  (Int -> Int -> Bool) -> -- direction predicate (compare segment y with point y)
+  (Int -> Int -> Int) -> -- combine (min or max)
+  Point ->
+  [Point] ->
+  Maybe Int
+shootRayVerticallyFrom dirPred combine point allPoints = go Nothing point relevantSegments
+  where
+    relevantSegments = filter (\((_, _), y) -> dirPred y (snd point)) $ horizontalSegments allPoints
+
+    go :: Maybe Int -> Point -> [HorizontalSegment] -> Maybe Int
+    go acc _ [] = acc
+    go acc p@(px, _) (((x1, x2), y) : qs)
+      | x1 <= px && px <= x2 = go (Just (maybe y (combine y) acc)) p qs
+      | otherwise = go acc p qs
+
+shootRayUpwardsFrom :: Point -> [Point] -> Maybe Int
+shootRayUpwardsFrom = shootRayVerticallyFrom (>) min
+
+shootRayDownwardsFrom :: Point -> [Point] -> Maybe Int
+shootRayDownwardsFrom = shootRayVerticallyFrom (<) max
+
+type VerticalSegment = (Int, (Int, Int)) -- (x, (y1, y2))
+
+verticalSegments :: [Point] -> [VerticalSegment]
+verticalSegments [] = []
+verticalSegments [_] = []
+verticalSegments ((x1, y1) : p2@(x2, y2) : ps)
+  | x1 == x2 = (x1, (minY, maxY)) : verticalSegments ps
+  | otherwise = verticalSegments (p2 : ps)
+  where
+    minY = min y1 y2
+    maxY = max y1 y2
+
+findRightmostVerticalSegment :: VerticalSegment -> [Point] -> VerticalSegment
+findRightmostVerticalSegment (px, (py1, py2)) allPoints = maximumBy (\(x1, _) (x2, _) -> compare x1 x2) relevantSegments
+  where
+    leftOfP :: VerticalSegment -> Bool
+    leftOfP = (< px) . fst
+
+    intersectingP :: VerticalSegment -> Bool
+    intersectingP (_, (qy1, qy2)) = intersects qy1 || intersects qy2
+      where
+        intersects y = min py1 py2 < y && y < max py1 py2
+
+    relevantSegments = filter (\seg -> leftOfP seg && intersectingP seg) $ verticalSegments allPoints
+
 -- By visual inspection of the input,
 -- part 2 must include either one of these points:
 --  (94870, 50025) or (94870, 48753)
-
-point1 :: (Int, Int)
+point1 :: Point
 point1 = (94870, 50025)
 
-point2 :: (Int, Int)
+point2 :: Point
 point2 = (94870, 48753)
 
-leftAbovePoint1 :: (Int, Int) -> Bool
-leftAbovePoint1 (x, y) = x < fst point1 && snd point1 < y
-
-leftBelowPoint2 :: (Int, Int) -> Bool
-leftBelowPoint2 (x, y) = x < fst point2 && y < snd point2
-
-partitionPoints :: [(Int, Int)] -> ([(Int, Int)], [(Int, Int)])
-partitionPoints [] = ([], [])
-partitionPoints (p : ps)
-  | leftAbovePoint1 p = let (xs, ys) = partitionPoints ps in (p : xs, ys)
-  | leftBelowPoint2 p = let (xs, ys) = partitionPoints ps in (xs, p : ys)
-  | otherwise = partitionPoints ps
-
 part2 :: Utils.SolutionSingle
-part2 input =
-  let points = Utils.splitInputToPair read "," input :: [(Int, Int)]
-      (subGroup1, subGroup2) = partitionPoints points
-      subGroup1Areas = map (area point1) $ trace (show subGroup1) subGroup1
-      subGroup2Areas = map (area point2) subGroup2
-   in maximum subGroup1Areas `max` maximum subGroup2Areas
+part2 input = point2Area `max` point1Area
+  where
+    points = Utils.splitInputToPair read "," input :: [Point]
+
+    point1MaxY = M.fromJust $ shootRayUpwardsFrom point1 points
+    point1VerticalSegment = (fst point1, (snd point1, point1MaxY))
+    (point1LeftSegmentX, point1LeftSegmentY) = findRightmostVerticalSegment point1VerticalSegment points
+    point1LeftY = if snd point1LeftSegmentY < snd point1 then snd point1LeftSegmentY else fst point1LeftSegmentY
+    point1Area = area point1 (point1LeftSegmentX, point1LeftY)
+
+    point2MinY = M.fromJust $ shootRayDownwardsFrom point2 points
+    point2VerticalSegment = (fst point2, (point2MinY, snd point2))
+    (point2LeftSegmentX, point2LeftSegmentY) = findRightmostVerticalSegment point2VerticalSegment points
+    point2LeftY = if snd point2 < snd point2LeftSegmentY then snd point2LeftSegmentY else fst point2LeftSegmentY
+    point2Area = area point2 (point2LeftSegmentX, point2LeftY)
 
 solution :: Utils.Solution
 solution = (part1, part2)
--- src/AoC/Day10.hs
+++ src/AoC/Day10.hs
@@ -1,15 +1,26 @@
 module AoC.Day10 (solution) where
 
 import Data.Char (isDigit)
+import Data.List (transpose)
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.List.Split (splitOn)
 import qualified Data.Sequence as Seq
 import Debug.Trace (trace)
 import qualified Utils.Utils as Utils
 
+-- PARSING --
+
+type IndicatorLight = [Bool]
+
+type ButtonWire = [Bool]
+
+type JoltageRequirement = [Int]
+
 data ParsedLine = ParsedLine
-  { indicatorLights :: [Bool],
-    buttonWires :: [[Bool]],
-    wireValues :: [Int]
+  { indicatorLights :: IndicatorLight,
+    buttonWires :: Seq.Seq ButtonWire,
+    numButtonWires :: Int,
+    joltageRequirement :: JoltageRequirement
   }
   deriving (Show)
 
@@ -24,32 +35,67 @@ parseLine line =
       middleParts = takeWhile (\s -> head s == '(') (tail parts)
       buttons = map (parseButtonWires len) middleParts
       lastPart = last parts
-      values = parseWireValues lastPart
-   in ParsedLine {indicatorLights = indicators, buttonWires = buttons, wireValues = values}
+      requirements = parseJoltageRequirement lastPart
+   in ParsedLine
+        { indicatorLights = indicators,
+          buttonWires = Seq.fromList buttons,
+          numButtonWires = length buttons,
+          joltageRequirement = requirements
+        }
 
 -- Parse [.##.] to [False, True, True, False]
-parseIndicatorLights :: String -> [Bool]
+parseIndicatorLights :: String -> IndicatorLight
 parseIndicatorLights s =
   let content = filter (\c -> c == '.' || c == '#') s
    in map (== '#') content
 
 -- Parse (3) or (1,3) to [False, False, False, True] or [False, True, False, True]
-parseButtonWires :: Int -> String -> [Bool]
+parseButtonWires :: Int -> String -> ButtonWire
 parseButtonWires len s =
   let content = filter (\c -> isDigit c || c == ',') s
       indices = map read (splitOn "," content) :: [Int]
    in [i `elem` indices | i <- [0 .. len - 1]]
 
 -- Parse {3,5,4,7} to [3,5,4,7]
-parseWireValues :: String -> [Int]
-parseWireValues s =
+parseJoltageRequirement :: String -> JoltageRequirement
+parseJoltageRequirement s =
   let content = filter (\c -> isDigit c || c == ',') s
    in map read (splitOn "," content)
+
+-- Generate all combinations of k elements from length n
+combinations :: Int -> Int -> [[Int]]
+combinations n = go 0
+  where
+    go :: Int -> Int -> [[Int]]
+    go i k
+      | k < 0 = error "k must be non-negative"
+      | n < k = error "k must not be greater than n"
+      | k == 0 = [[]]
+      | k == n - i = [[i .. n - 1]]
+      | otherwise = map (i :) (go (i + 1) (k - 1)) ++ go (i + 1) k
+
+-- SOLUTION --
+
+pressButtonsResult :: [ButtonWire] -> IndicatorLight
+pressButtonsResult = map (NonEmpty.xor . NonEmpty.fromList) . transpose
+
+getButtons :: [Int] -> Seq.Seq ButtonWire -> [ButtonWire]
+getButtons indices wires = map (Seq.index wires) indices
+
+fewestButtonPresses :: ParsedLine -> Int
+fewestButtonPresses parsedLine = go 1
+  where
+    go :: Int -> Int
+    go presses = if result then presses else go (presses + 1)
+      where
+        pressCombinations = combinations (numButtonWires parsedLine) presses
+        buttonCombinations = map (`getButtons` buttonWires parsedLine) pressCombinations
+        result = foldl (\acc press -> acc || pressButtonsResult press == indicatorLights parsedLine) False buttonCombinations
 
 part1 :: Utils.SolutionSingle
 part1 input =
   let parsedInput = parsing input
-   in trace (show parsedInput) 0
+   in sum $ map fewestButtonPresses parsedInput
 
 part2 :: Utils.SolutionSingle
 part2 _ = 0
